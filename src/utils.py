9d38b2c2adcd5ecdfa9e190bbd5510abde16cfa7
Vaishnavi Gupta <Vaishnavi.ds2003@gmail.com>
2026-06-05 13:02:34 +0530

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import numpy as np
 import dill
 from sklearn.metrics import r2_score
 from src.exception import CustomException
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -17,12 +18,19 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_model(X_train, y_train,X_test,y_test, models):
+def evaluate_model(X_train, y_train,X_test,y_test, models, params):
+
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train, y_train)
+            para=params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train) 
 
             y_train_pred = model.predict(X_train)   
             y_test_pred = model.predict(X_test)
@@ -37,3 +45,10 @@ def evaluate_model(X_train, y_train,X_test,y_test, models):
     except Exception as e:
         raise CustomException(e, sys)
     
+def load_object(file_path):
+        try:
+            with open(file_path,'rb') as file_obj:
+                return dill.load(file_obj)
+        except Exception as e:
+            raise CustomException(e,sys)
+        
